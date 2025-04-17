495eb578a724c55f774c68daa82770f94418df43
Ray <138245350+XrayWizz@users.noreply.github.com>
2025-04-17 14:07:08 +0200

    Update Omnis-Novus.lua

--- Omnis-Novus.lua
+++ Omnis-Novus.lua
@@ -1,1 +1,39 @@
+-- Omnis-Novus Interface Overhaul Script
+local Players = game:GetService("Players")
+local StarterGui = game:GetService("StarterGui")
+
+-- Create the confirmation prompt
+local function showConfirmationPrompt()
+    local bindable = Instance.new("BindableFunction")
+    
+    function bindable.OnInvoke(response)
+        if response == "Yes" then
+            -- Place for the interface overhaul code
+            print("Starting interface overhaul...") -- Placeholder
+            -- TODO: Add your interface modification code here
+        else
+            print("Script cancelled by user")
+        end
+    end
+
+    StarterGui:SetCore("SendNotification", {
+        Title = "Omnis-Novus",
+        Text = "Are you sure you want to run this script?",
+        Duration = 10,
+        Callback = bindable,
+        Button1 = "Yes",
+        Button2 = "No"
+    })
+end
+
+-- Function to initialize the script
+local function init()
+    local player = Players.LocalPlayer
+    if player then
+        showConfirmationPrompt()
+    end
+end
+
+-- Run the initialization
+init()
 
